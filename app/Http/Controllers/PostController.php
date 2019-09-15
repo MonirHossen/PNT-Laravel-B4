1d477d6bd58f2ef63dcb18e5983071e155a6e63d
Monir Hossen <mdmonirhossen929@gmail.com>
2019-09-15 14:09:23 +0600

    Post Creation Done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    //
+   public function create(){
+        return view('post/create');
+   }
+   public function store(Request $request){
+            $data['title']   =  $request->title;
+            $data['details'] =  $request->details;
+            DB::table('posts')->insert($data);
+            return redirect('post/create');
+
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('post/create','PostController@create');
+Route::post('post/store','PostController@store');
